1034ce4af11ade0ee5aee71f1e34a080c1097e4f
LouCypher <zoolcar9+dev@gmail.com>
2013-10-17 06:52:04 +0700

    eggs

---
+++ src/defaults/preferences/preferences.js
@@ -0,0 +1,2 @@
+pref("extensions.aboutnewtab-contextmenu@loucypher.description", "chrome://newtabcontext/locale/newtabcontext.properties");
+pref("extensions.aboutnewtab-contextmenu.kojiyuu", false);
